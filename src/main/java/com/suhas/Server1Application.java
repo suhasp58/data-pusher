c060ad2a2e59d1fda92edd4090f474cf00ded535
suhasp58 <52521805+suhasp58@users.noreply.github.com>
2019-07-23 16:03:49 +0530

    Add files via upload

--- src/main/java/com/suhas/Server1Application.java
+++ src/main/java/com/suhas/Server1Application.java
@@ -1,13 +1,25 @@
 package com.suhas;
 
+import java.io.IOException;
+import java.sql.Date;
+import java.util.TimerTask;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Server1Application {
-
-	public static void main(String[] args) {
-		SpringApplication.run(Server1Application.class, args);
-	}
+	
+	
+	
+	
+	
+	private static void openHomePage() throws IOException {
+	       Runtime rt = Runtime.getRuntime();
+	       rt.exec("rundll32 url.dll,FileProtocolHandler " + "http://localhost:8085/redisdata");
+	    }
 
 }
--- src/main/java/com/suhas/controller.java
+++ src/main/java/com/suhas/controller.java
@@ -1,33 +1,39 @@
 package com.suhas;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Properties;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.springframework.boot.SpringApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import com.suhas.Server1Application;
 import redis.clients.jedis.Jedis;
 
-@RestController
+//@RestController
 public class controller {
-
-	@RequestMapping("redisdata")
-	public String sayHello() throws Exception {
+	//@RequestMapping("redisdata")
+	public static String sayHello() throws Exception {
+		Server1Application sa = new Server1Application();
+        
 		Properties prop = new Properties();
 		InputStream input = controller.class.getClassLoader().getResourceAsStream("application.properties");
 		prop.load(input);
-		System.out.println("Prop-->" + prop.getProperty("filename"));
+		//System.out.println("Prop-->" + prop.getProperty("filename"));
 		String data = new String(Files.readAllBytes(Paths.get(prop.getProperty("filename"))));
 		Jedis jedis = new Jedis(prop.getProperty("ip"), 6379);
 		try {
 
 			JSONObject obj = new JSONObject(data);
 			//JSONArray n = obj.getJSONArray("instance");
-			System.out.println(obj);
+			//System.out.println(obj);
 			
 
 				String key = "10.37.56.1";
@@ -44,4 +50,22 @@ public class controller {
 		}
 		return "INSERTED SUCCESSFULLY.";
 	}
+	public static void main(String[] args) throws IOException {
+		SpringApplication.run(Server1Application.class, args);
+		ScheduledExecutorService ses = Executors.newSingleThreadScheduledExecutor();
+		ses.scheduleAtFixedRate(new Runnable() {
+		    @Override
+		    public void run() {
+		       try {
+		    	  sayHello();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		    }
+		}, 0, 10, TimeUnit.SECONDS);
+}
 }
